bac530049cf42ce66e315293c694f0f36d30fe99
xoccoco <164484018+xoccoco@users.noreply.github.com>
2024-06-18 13:46:48 +0200

    upload classlist toggle

---
+++ classList_toggle.js
@@ -0,0 +1,11 @@
+let boton = document.getElementById("boton");
+
+
+function menu(){
+
+    document.getElementById("lista").classList.toggle("estado");
+    
+}
+
+boton.addEventListener("click", menu);
+
